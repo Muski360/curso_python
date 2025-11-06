8273819d046d079f5e63b50b399244e0cf6f55ab
Muski360 <mudb360@gmail.com>
2025-11-05 21:39:52 -0300

    Commit da aula06

--- Aulas/aula06/baskaraST.py
+++ Aulas/aula06/baskaraST.py
@@ -1,30 +1,30 @@
-import streamlit as st 
-import math as mt 
+from streamlit import header, write, button, warning, success, error, text_input
+from math import sqrt
 
-st.header("Calculadora de Bhaskara")
-st.write("Calculadora de raízes \n de uma equação do segundo grau")
-st.write("ax² + bx + c = 0")
+header("Calculadora de Bhaskara")
+write("Calculadora de raízes \n de uma equação do segundo grau")
+write("ax² + bx + c = 0")
 
 #Entrada de dados 
-a = text_input = st.number_input("Digite o valor de a:", value=1.0)
-b = text_input = st.number_input("Digite o valor de b:", value=0.0)
-c = text_input = st.number_input("Digite o valor de c:", value=0.0) 
+a = text_input("Digite o valor de a:")
+b = text_input("Digite o valor de b:")
+c = text_input("Digite o valor de c:")
 
 #Processamento de dados 
-if st.button("Calcular raízes"):
+if button("Calcular raízes"):
     try:
         a = float(a)
         b = float(b)
         c = float(c)
         delta = b**2 - 4*a*c
         if delta < 0:
-            st.warning("A equação não possui raízes reais.")
+            warning("A equação não possui raízes reais.")
         elif delta == 0:
             raiz = -b / (2*a)
-            st.success(f"A equação possui uma raiz real: x = {raiz:.2f}")
+            success(f"A equação possui uma raiz real: x = {raiz:.2f}")
         else:
-            raiz1 = (-b + mt.sqrt(delta)) / (2*a)
-            raiz2 = (-b - mt.sqrt(delta)) / (2*a)
-            st.success(f"A equação possui duas raízes reais: x1 = {raiz1:.2f} e x2 = {raiz2:.2f}")
+            raiz1 = (-b + sqrt(delta)) / (2*a)
+            raiz2 = (-b - sqrt(delta)) / (2*a)
+            success(f"A equação possui duas raízes reais: x1 = {raiz1:.2f} e x2 = {raiz2:.2f}")
     except ValueError:
-        st.error("Por favor, insira valores numéricos válidos para a, b e c.")
+        error("Por favor, insira valores numéricos válidos para a, b e c.")
